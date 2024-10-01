6d3c21dc8e53db29680bf77c3f77755dcf58c80d
Milos Medic <milosmedicdev@gmail.com>
2024-10-01 09:18:59 +0200

    handle csrf-token

--- src/services/baseQuery.ts
+++ src/services/baseQuery.ts
@@ -12,6 +12,8 @@ const baseUrl = `${API_BASE_URL}`;
 // Create a new mutex
 const mutex = new Mutex();
 
+let csrfToken: string | null = null;
+
 const baseQuery = fetchBaseQuery({
   baseUrl,
   prepareHeaders: (headers) => {
@@ -20,26 +22,72 @@ const baseQuery = fetchBaseQuery({
     if (apiKey) {
       headers.set("X-API-KEY", `${apiKey}`);
       headers.set("Accept", "application/json");
+
+      if (csrfToken) {
+        headers.set("X-CSRF-Token", csrfToken);
+      }
     }
     return headers;
   },
 });
+
+const fetchCSRFToken = async () => {
+  // const response = await fetch(`https://httpbin.org/get`, {
+  const response = await fetch(`${baseUrl}csrf-token-endpoint`, {
+    method: "GET",
+    headers: {
+      "X-API-KEY": store.getState().appReducer.apiKey || "",
+      Accept: "application/json",
+    },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    // return data.origin;
+    return data.csrfToken;
+  }
+
+  throw new Error("Failed to fetch CSRF token");
+};
 
 const customFetchBase: BaseQueryFn<
   string | FetchArgs,
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
-  // wait until the mutex is available without locking it
   await mutex.waitForUnlock();
+
+  if (!csrfToken) {
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire();
+      try {
+        csrfToken = await fetchCSRFToken();
+      } catch (error) {
+        console.error("Error fetching CSRF token:", error);
+      } finally {
+        release();
+      }
+    } else {
+      await mutex.waitForUnlock();
+    }
+  }
+
   let result = await baseQuery(args, api, extraOptions);
+
   if (result.error && result.error.status === 401) {
     if (!mutex.isLocked()) {
       const release = await mutex.acquire();
 
-      release();
+      try {
+        csrfToken = await fetchCSRFToken();
+      } catch (error) {
+        console.error("Error refreshing CSRF token:", error);
+      } finally {
+        release();
+      }
+
+      result = await baseQuery(args, api, extraOptions);
     } else {
-      // wait until the mutex is available without locking it
       await mutex.waitForUnlock();
       result = await baseQuery(args, api, extraOptions);
     }
